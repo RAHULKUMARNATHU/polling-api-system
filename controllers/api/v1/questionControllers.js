c97197ec6e7b3c908230fc0120a8a06e7f025d26
Rahul <rahulvaidya148@gmail.com>
2022-06-02 16:26:42 +0530

    added view in questionController

--- controllers/api/v1/questionControllers.js
+++ controllers/api/v1/questionControllers.js
@@ -1,32 +1,40 @@
-
 // import models
-const Question = require('../../../models/questions');
+const Question = require("../../../models/questions");
 
 // create question
-module.exports.createQuestion = async function(req, res){
+module.exports.createQuestion = async function (req, res) {
+  try {
+    let question = await Question.create(req.body);
 
-     
-
-    try{
-        let question = await Question.create(req.body);
-
-        // if question is created then return json response
-        if(question){
-            return res.json({
-                question,
-                data : { message : "question created successfully" },
-            });
-        }else{
-            // if no question created then error in response status
-            return res.status(500).json({
-                data : { message : "Internal server error" }
-            });
-        }
-    }catch(err){
-        console.log("Error while creating question", err);
-        return;
+    // if question is created then return json response
+    if (question) {
+      return res.json({
+        question,
+        data: { message: "question created successfully" },
+      });
+    } else {
+      // if no question created then error in response status
+      return res.status(500).json({
+        data: { message: "Internal server error" },
+      });
     }
-   
-}
+  } catch (err) {
+    console.log("Error while creating question", err);
+    return;
+  }
+};
 
+// view questions
+module.exports.viewQuestions = async function (req, res) {
+  try {
+    let question = await Question.findById(req.params.id).populate("options");
 
+    return res.json({ question });
+  } catch (err) {
+    return res.status(500).json({
+      data: {
+        message: "Internal server error viewing question",
+      },
+    });
+  }
+};
--- routes/api/v1/question.js
+++ routes/api/v1/question.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const questionController = require('../../../controllers/api/v1/questionControllers');
 
 router.post('/create', questionController.createQuestion);
-
+router.get('/:id', questionController.viewQuestions);
 module.exports = router;
