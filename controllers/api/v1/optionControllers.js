6ee71db28a6e5d518b45f68f9e37c6084c703fe2
Rahul <rahulvaidya148@gmail.com>
2022-06-03 14:13:55 +0530

    Commited Upto Delete Option

--- controllers/api/v1/optionControllers.js
+++ controllers/api/v1/optionControllers.js
@@ -15,6 +15,8 @@ module.exports.createOption = async function (req, res) {
       option.link_vote =
         "http://localhost:8000/api/v1/options/" + option.id + "add_vote";
       option.save();
+      question.options.push(option);
+      question.save();
 
       return res.json({
         option,
@@ -27,5 +29,37 @@ module.exports.createOption = async function (req, res) {
   } catch (err) {
     console.log("Error : ", err);
     return;
+  }
+};
+
+// delete an option on the basis of its id
+module.exports.optionDelete = async function (req, res) {
+  try {
+    let id = req.params.id;
+
+    // finding the option exists or not
+    let option = await Option.findById(id);
+
+    // if option is present then check it has vote or not
+    // if option has vote then not have to delete it
+    if (option.votes > 0) {
+      return res.status(404).json({
+        data: { message: "can't delete it! It has vote" },
+      });
+    }
+    // delete option from Question.options array also
+    await Question.findByIdAndUpdate(option.question, {
+      $pull: { options : id },
+    });
+
+    // else delete the option from option
+    await Option.findByIdAndDelete(id);
+    return res.status(200).json({
+      data: { message: "Option deleted sucessfully" },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      data: { message: "Internal server error" },
+    });
   }
 };
--- routes/api/v1/option.js
+++ routes/api/v1/option.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const optionController = require('../../../controllers/api/v1/optionControllers');
 
 router.post('/:id/options/create',optionController.createOption);
+router.delete('/:id/delete', optionController.optionDelete);
 
 
 module.exports = router;
